407ecb5e77950bc2c7db8184249607984308efb3
มานิต จิตต์ประไพย <manit.pbac@gmail.com>
2026-06-30 09:38:30 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["hardness-ducking-kindred.ngrok-free.dev"],
+  allowedDevOrigins: ["basinuat.horusai.pro"],
 };
 
 export default nextConfig;
